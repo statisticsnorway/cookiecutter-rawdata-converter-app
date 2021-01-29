f159d1115bf456565e11fac5d8ae6c0cf5068f8e
Kenneth Leine Schulstad <ks@udp.no>
2021-01-29 12:16:37 +0100

    Add RawdataConverterTest

---
+++ {{cookiecutter.project_name}}/src/test/java/{{cookiecutter.package_dir}}/{{cookiecutter.project_slug}}/{{cookiecutter.converter_name}}RawdataConverterTest.java
@@ -0,0 +1,35 @@
+package {{cookiecutter.base_package}}.{{cookiecutter.project_slug}};
+
+import no.ssb.rawdata.converter.core.convert.ConversionResult;
+import no.ssb.rawdata.converter.core.convert.ValueInterceptorChain;
+import no.ssb.rawdata.converter.test.message.RawdataMessageFixtures;
+import no.ssb.rawdata.converter.test.message.RawdataMessages;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+@Disabled
+public class {{cookiecutter.converter_name}}RawdataConverterTest {
+
+    static RawdataMessageFixtures fixtures;
+
+    @BeforeAll
+    static void loadFixtures() {
+        fixtures = RawdataMessageFixtures.init("sometopic");
+    }
+
+    @Disabled
+    @Test
+    void shouldConvertRawdataMessages() {
+        RawdataMessages messages = fixtures.rawdataMessages("sometopic"); // TODO: replace with topicname
+        {{cookiecutter.converter_name}}RawdataConverterConfig config = new {{cookiecutter.converter_name}}RawdataConverterConfig();
+        // TODO: Set app config
+
+        {{cookiecutter.converter_name}}RawdataConverter converter = new {{cookiecutter.converter_name}}RawdataConverter(config, new ValueInterceptorChain());
+        converter.init(messages.index().values());
+        ConversionResult res = converter.convert(messages.index().get("123456")); // TODO: replace with message position
+    }
+
+}
---
+++ {{cookiecutter.project_name}}/src/test/resources/rawdata-messages/sometopic/README.md
@@ -0,0 +1,5 @@
+Add position directories with rawdata contents in here...
+
+These files are referenced from {{cookiecutter.converter_name}}RawdataConverterTest.java
+
+TODO: Remove this file :-)
